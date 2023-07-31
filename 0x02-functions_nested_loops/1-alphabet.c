d1291aa31f3810f128ac2d49b347b1d0b770409e
tg4553t <tigistse@example.com>
2023-07-31 02:05:55 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,11 @@
 #include "main.h"
-void print_alphabet(void){
-   char ch = 'a';
-   while(ch <= 'z'){
-	   _putchar(ch);
-	   ch++;
-   }
-   _putchar('\n');
+void print_alphabet(void)
+{
+     char ch = 'a';
+     while (ch <= 'z')
+     {
+	     _putchar(ch);
+	     ch++;
+     }
+     _putchar('\n');
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,17 @@
 #include "main.h"
-void print_alphabet_x10(void){
+/**
+ * print_alphabet_x10 - Read charcters and prints using _char
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10 (void)
+{
   int i;
-  for (i=1; i<=10; i++){
-      char ch='a';
-      while(ch <= 'z'){
+  for (i = 1; i <= 10; i++)
+  {
+      char ch = 'a';
+      while (ch <= 'z')
+      {
 	      _putchar(ch);
 	      ch++;
       }
