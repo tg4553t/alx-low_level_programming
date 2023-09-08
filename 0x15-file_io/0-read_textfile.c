a3748a1925ffd104b5f2f25fda5048d45b276c75
tg4553t <hnewme5e@gmail.com>
2023-09-08 04:18:08 -0700

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,17 +7,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	ssize_t br, bw, fd;
-	fd = open (filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	br = read(fd, buf, letters);
-	if (br == - 1 || buf == NULL)
+	if (br == -1 || buf == NULL)
 		return (0);
 	bw = write(STDOUT_FILENO, buf, br);
 	if (bw == -1 || bw != br)
 		return (0);
 	free(buf);
 	close(fd);
-	return(bw);	
+	return (bw);	
 }
