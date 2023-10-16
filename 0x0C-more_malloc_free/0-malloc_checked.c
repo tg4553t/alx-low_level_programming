1a57ff9bc86bd229e4521ae948f594b60df1372d
Tigist <tigistse@gmail.com>
2023-10-16 09:11:02 +0300

    Delete 0x0C-more_malloc_free directory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-void *malloc_checked(unsigned int b)
-{
-	void *assign;
-	assign = malloc(b);
-	if (assign == NULL )
-		exit(98);
-	return(assign);
-}
--- 0x0C-more_malloc_free/_putchar.c
+++
@@ -1,6 +0,0 @@
-#include <unistd.h>
-
-int _putchar(char_c) 
-{
-	return (write (1, &c, 1));
-}
